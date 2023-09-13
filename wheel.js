3a8f8b3efcf869679c665bf3dfb7f074c4fce28d
goolgemer <75084707+goolgemer@users.noreply.github.com>
2023-09-13 22:19:30 +0700

    x
    x

--- wheel.js
+++ wheel.js
@@ -1,14 +1,69 @@
 let container = document.querySelector('.container');
-let wheelBG = document.querySelector('.wheel_bg')
+let wheelBG = document.querySelector('.wheel_bg');
 let btn = document.getElementById('spin');
+let spinDeg = '3000deg';
+let clockwise = true;
+let animationRunning = false; 
 
+btn.addEventListener("click", (event) => {
+  
+  if (animationRunning) {
+       stopAnimation();
+  }
 
-
-
+  
+  else {
+    justSpin(spinDeg);
+  }
    
+});
 
-window.addEventListener("load", (event) => {
-    container.style.transform = "rotate(3000deg)";
-    wheelBG.style.transform = "rotate(3000deg)";
+function justSpin(deg = "0deg") {
+  container.style.transition = "5s";
+  wheelBG.style.transition = "5s";
+  container.style.transform = `rotate(${deg})`;
+  wheelBG.style.transform = `rotate(${deg})`;
+  animationRunning = false;
+}
 
-  });
+
+function startAnimation(deg = "0deg") {
+  container.style.transition = "5s";
+  wheelBG.style.transition = "5s";
+  container.style.transform = `rotate(${deg})`;
+  wheelBG.style.transform = `rotate(${deg})`;
+  animationRunning = true;
+}
+
+function stopAnimation() {
+  container.style.transition = "none"; 
+  wheelBG.style.transition = "none";
+  animationRunning = false;
+}
+
+function spin() {
+  setTimeout(() => {
+    container.style.transition = "1s";
+    wheelBG.style.transition = "1s";
+    if (clockwise) {
+      container.style.transform = "rotate(-90deg)";
+      wheelBG.style.transform = "rotate(-90deg)";
+    } else {
+      container.style.transform = "rotate(90deg)";
+      wheelBG.style.transform = "rotate(90deg)";
+    }
+   
+    clockwise = !clockwise;
+    
+    if (animationRunning) {
+      spin();
+    }else{
+      justSpin(spinDeg);
+    }
+  }, 1000);
+}
+
+setTimeout(() => {
+  startAnimation();
+  spin();
+}, 1000);
