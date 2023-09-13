4aa786085be5e9ad3a27b95b52e9ec22eeb7cfa7
goolgemer <75084707+goolgemer@users.noreply.github.com>
2023-09-13 15:38:50 +0700

    Add files via upload

---
+++ wheel.js
@@ -0,0 +1,14 @@
+let container = document.querySelector('.container');
+let wheelBG = document.querySelector('.wheel_bg')
+let btn = document.getElementById('spin');
+
+
+
+
+   
+
+window.addEventListener("load", (event) => {
+    container.style.transform = "rotate(3000deg)";
+    wheelBG.style.transform = "rotate(3000deg)";
+
+  });
