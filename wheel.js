c76cede00967a8692ab2d5ec98d472840c5a3403
goolgemer <75084707+goolgemer@users.noreply.github.com>
2023-09-15 00:20:32 +0700

    4
    4

--- wheel.js
+++ wheel.js
@@ -1,9 +1,10 @@
 let container = document.querySelector('.container');
 let wheelBG = document.querySelector('.wheel_bg');
 let btn = document.getElementById('spin');
-let spinDeg = '3000deg';
+let spinDeg = '3036deg';
 let clockwise = true;
 let animationRunning = false; 
+let popup = document.querySelector('.popup');
 
 btn.addEventListener("click", (event) => {
   
@@ -39,6 +40,9 @@ function stopAnimation() {
   container.style.transition = "none"; 
   wheelBG.style.transition = "none";
   animationRunning = false;
+  setTimeout(() => {
+    popup.classList.add('popup_active');
+  },6000);
 }
 
 function spin() {
@@ -67,3 +71,11 @@ setTimeout(() => {
   startAnimation();
   spin();
 }, 1000);
+
+
+
+
+
+// 8 сектаров
+// POP-UP
+//Кнопка в центре
